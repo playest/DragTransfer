538bd1a57b52fb0d31b0ec7e7feb7d6c321c4f22
playest <playest@noemail.com>
2022-01-12 01:14:37 +0100

    Trying to adjust the quantity on both side.
    Kind of working but I need some kind of flag if I want to ask for the amount to be transfered.

--- DragTransfer.js
+++ DragTransfer.js
@@ -11,16 +11,23 @@ Hooks.once('init', () => {
     registerSettings();
 });
 
-Hooks.on('dropActorSheetData', (dragTarget, sheet, dragSource, user) => {
+/*
+options: {"temporary":false, "renderSheet":false, "render":true}
+*/
+Hooks.on('createItem', (createdItem, options, userId, d, e) => {
+    console.log("createItem", createdItem, options, userId, d, e, this);
+});
+
+Hooks.on('dropActorSheetData', (dragTargetActor, sheet, dragSourceItem) => {
     function isAlt() {
         // check if Alt and only Alt is being pressed during the drop event.
         const alts = new Set(["Alt", "AltLeft"]);
         return (game.keyboard.downKeys.size == 1 && game.keyboard.downKeys.intersects(alts));
     }
-    
+
     function checkCompatible(actorTypeName1, actorTypeName2) {
-        console.info('DragNTransfer - Check Compatibility: Dragging Item:"' + String(dragSource.data.type) + '" from sourceActor.data.type:"' + String(actorTypeName1) + '" to dragTarget.data.type:"' + String(actorTypeName2) + '".');
-    
+        console.info('DragNTransfer - Check Compatibility: Dragging Item:"' + String(dragSourceItem.data.type) + '" from sourceActor.data.type:"' + String(actorTypeName1) + '" to dragTarget.data.type:"' + String(actorTypeName2) + '".');
+
         const transferBetweenSameTypeActors = game.settings.get('DragTransfer', 'actorTransferSame');
         if(transferBetweenSameTypeActors && actorTypeName1 == actorTypeName2) {
             return true;
@@ -45,24 +52,41 @@ Hooks.on('dropActorSheetData', (dragTarget, sheet, dragSource, user) => {
         return;  // ignore Drag'N'Transfer when Alt is pressed to drop.
     }
 
-    if(dragSource.type == "Item" && dragSource.actorId) {
-        if(!dragTarget.data._id) {
-            console.warn("Drag'n'Transfer - target has no data._id?", dragTarget);
+    if(dragSourceItem.type == "Item" && dragSourceItem.actorId) {
+        if(!dragTargetActor.data._id) {
+            console.warn("Drag'n'Transfer - target has no data._id?", dragTargetActor);
             return;
         }
-        if(dragTarget.data._id == dragSource.actorId) {
+        if(dragTargetActor.data._id == dragSourceItem.actorId) {
             return;  // ignore dropping on self
         }
-        let sourceActor = game.actors.get(dragSource.actorId);
+        let sourceActor = game.actors.get(dragSourceItem.actorId);
         if(sourceActor) {
             /* if both source and target have the same type then allow deleting original item. this is a safety check because some game systems may allow dropping on targets that don't actually allow the GM or player to see the inventory, making the item inaccessible. */
 
-            if(checkCompatible(sourceActor.data.type, dragTarget.data.type)) {
-                if(sourceActor.deleteEmbeddedDocuments != undefined) {
-                    sourceActor.deleteEmbeddedDocuments("Item", [dragSource.data._id]);
+            if(checkCompatible(sourceActor.data.type, dragTargetActor.data.type)) {
+                if(dragSourceItem.data.data.quantity > 1) {
+                    let transferedQuantity = 2;
+                    const sourceItem = game.actors.get(dragSourceItem.actorId).items.get(dragSourceItem.data._id);
+                    dragSourceItem.data.data.quantity = 100;
+                    const updatedSourceQuantity = sourceItem.data.data.quantity - transferedQuantity;
+                    sourceItem.update({"data.quantity": updatedSourceQuantity});
+                    if(updatedSourceQuantity <= 0) {
+                        if(sourceActor.deleteEmbeddedDocuments != undefined) {
+                            sourceActor.deleteEmbeddedDocuments("Item", [dragSourceItem.data._id]);
+                        }
+                        else {
+                            sourceActor.deleteOwnedItem(dragSourceItem.data._id);
+                        }
+                    }
                 }
                 else {
-                    sourceActor.deleteOwnedItem(dragSource.data._id);
+                    if(sourceActor.deleteEmbeddedDocuments != undefined) {
+                        sourceActor.deleteEmbeddedDocuments("Item", [dragSourceItem.data._id]);
+                    }
+                    else {
+                        sourceActor.deleteOwnedItem(dragSourceItem.data._id);
+                    }
                 }
             }
         }
