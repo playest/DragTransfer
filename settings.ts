5aa6fdfeca18b8a9c92882de467f0a7fce2f8f6c
playest <playest@noemail.com>
2022-01-13 17:11:09 +0100

    unadd js files and add settings.ts (forgot that after moving it)

---
+++ settings.ts
@@ -0,0 +1,33 @@
+"use strict";
+
+export function registerSettings() {
+    const moduleName = 'DragTransfer';
+    const MODNAME = 'DRAGTANSFER';
+
+    game.settings.register(moduleName, 'actorTransferSame', {
+        name: game.i18n.localize(MODNAME + ".actorTransferSame"),
+        hint: game.i18n.localize(MODNAME + ".actorTransferSameHint"),
+        scope: 'world',
+        config: true,
+        type: Boolean,
+        default: true,
+    });
+
+    game.settings.register(moduleName, 'actorTransferPairs', {
+        name: game.i18n.localize(MODNAME + ".actorTransferPairs"),
+        hint: game.i18n.localize(MODNAME + ".actorTransferPairsHint"),
+        scope: 'world',
+        config: true,
+        type: String,
+        default: "",
+        onChange: (value: string) => {
+            try {
+                JSON.parse("{" + value + "}");
+            }
+            catch(err: any) {
+                ui.notifications.error(err.message);
+                throw err;
+            }
+        }
+    });
+};
