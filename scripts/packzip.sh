713616b7aa6428da76f4378ddc163fe064d5c0a6
playest <playest@noemail.com>
2022-01-13 23:14:58 +0100

    add packzip.sh script

---
+++ scripts/packzip.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+if [ ! "$npm_package_name" ]
+then
+    echo "Since \$npm_package_name is empty it probably means that you tan this script from outside of npm. We will try to find values for the needed npm variables." >&2
+    npm_package_name=`jq -r '.name' package.json`
+    npm_package_version=`jq -r '.version' package.json`
+    if [ "$pack_archive_name" = "-.tgz" ]
+    then
+        echo "Could no find value for the needed npm variables. Aborting." >&2
+        exit 1
+    fi
+fi
+
+npm pack
+
+pack_archive_name="$npm_package_name-$npm_package_version.tgz"
+
+if [ -f "$pack_archive_name" ]
+then
+    echo "Pack archive ($pack_archive_name) detected."
+    tar xzf "$pack_archive_name" -C "releases/"
+    cd "releases/"
+    mv "package" "$npm_package_name-$npm_package_version"
+    zip -r "$npm_package_name-$npm_package_version.zip" "$npm_package_name-$npm_package_version"
+    cd ..
+    rm -r "releases/$npm_package_name-$npm_package_version"
+else
+    echo "Pack archive ($pack_archive_name) not found. Will not do anything."
+fi
+
+rm "$pack_archive_name"
