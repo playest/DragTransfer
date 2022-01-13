3fef317731546840b7551b6df8dce8ea22f6f586
playest <playest@noemail.com>
2022-01-13 23:26:14 +0100

    Change regex for version number to support version with multiple digits such as 1.2.123. Change archive name for download to .zip

--- scripts/check_same_version.sh
+++ scripts/check_same_version.sh
@@ -20,24 +20,24 @@ then
     err=2
 fi
 
-module_json_manifest_version=`echo $module_json_manifest | grep -o '/releases/download/v[0-9]\+\.[0-9]\+\.[1-9]\+/' | cut -d "/" -f 4 | cut -c 2-`
+module_json_manifest_version=`echo $module_json_manifest | grep -o '/releases/download/v[0-9]\+\.[0-9]\+\.[0-9]\+/' | cut -d "/" -f 4 | cut -c 2-`
 if [ "$module_json_manifest_version" != "$package_json_version" ]
 then
     echo "Different version number in package.json ($package_json_version) and module.json/manifest ($module_json_manifest_version)." >&2
     err=3
 fi
 
-module_json_download_version=`echo $module_json_download | grep -o '/releases/download/v[0-9]\+\.[0-9]\+\.[1-9]\+/' | cut -d "/" -f 4 | cut -c 2-`
+module_json_download_version=`echo $module_json_download | grep -o '/releases/download/v[0-9]\+\.[0-9]\+\.[0-9]\+/' | cut -d "/" -f 4 | cut -c 2-`
 if [ "$module_json_download_version" != "$package_json_version" ]
 then
     echo "Different version number in package.json ($package_json_version) and module.json/download ($module_json_download_version)." >&2
     err=4
 fi
 
-module_json_download_tgz_version=`echo $module_json_download | grep -o -- '-[0-9]\+\.[0-9]\+\.[1-9]\+\.tgz$' | cut -c 2- | sed 's/\.tgz$//'`
-if [ "$module_json_download_tgz_version" != "$package_json_version" ]
+module_json_download_archive_version=`echo $module_json_download | grep -o -- '-[0-9]\+\.[0-9]\+\.[0-9]\+\.zip$' | cut -c 2- | sed 's/\.zip$//'`
+if [ "$module_json_download_archive_version" != "$package_json_version" ]
 then
-    echo "Different version number in package.json ($package_json_version) and module.json/download/tgz ($module_json_download_version)." >&2
+    echo "Different version number in package.json ($package_json_version) and module.json/download/archive ($module_json_download_archive_version)." >&2
     err=5
 fi
 
